bd94db88ca2adbbe0cef3dca459ad01800cf7b8a
San Valen <osoriovalentin5@gmail.com>
2024-06-03 19:15:15 -0500

    Add swagger documentation to new create order

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,11 +1,17 @@
 import { Type } from 'class-transformer';
 import { ArrayMinSize, IsArray, ValidateNested } from 'class-validator';
 import { OrderItemDto } from './order-item.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateOrderDto {
   @IsArray()
   @ArrayMinSize(1)
   @ValidateNested({ each: true })
   @Type(() => OrderItemDto)
+  @ApiProperty({
+    isArray: true,
+    type: OrderItemDto,
+    required: true,
+  })
   items!: OrderItemDto[];
 }
--- src/orders/dto/order-item.dto.ts
+++ src/orders/dto/order-item.dto.ts
@@ -1,18 +1,34 @@
 import { IsInt, IsOptional, IsPositive } from 'class-validator';
 import { IOrderItem } from '../entities/order-item.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 interface IOrderItemDto extends Omit<IOrderItem, 'id' | 'orderId'> {}
 
 export class OrderItemDto implements IOrderItemDto {
   @IsInt()
   @IsPositive()
+  @ApiProperty({
+    example: 1,
+    description: 'Product ID.',
+    required: true,
+  })
   productId!: number;
 
   @IsInt()
   @IsPositive()
+  @ApiProperty({
+    example: 5,
+    description: 'Quantity of the product.',
+    required: true,
+  })
   quantity!: number;
 
   @IsInt()
   @IsOptional()
+  @ApiProperty({
+    example: 1000,
+    description: 'Desired Price (is ignored :D).',
+    required: false,
+  })
   price!: number;
 }
