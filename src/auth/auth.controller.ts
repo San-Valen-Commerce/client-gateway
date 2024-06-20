3d7d55e4bb3fec650847cafcfe314f09addd4ee1
San Valen <osoriovalentin5@gmail.com>
2024-06-19 23:09:33 -0500

    Implement auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Get, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post, UseGuards } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 import { NATS_SERVICE } from 'src/config';
+import { LoginUserDto, RegisterUserDto } from './dto';
+import { AuthGuard } from './guards/auth.guard';
+import { Token, User } from './decorators';
+import { CurrentUser } from './interfaces/current-user.interface';
 
 @Controller({
   path: 'auth',
@@ -11,10 +15,10 @@ export class AuthController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post('register')
-  async registerUser() {
+  async registerUser(@Body() registerUserDto: RegisterUserDto) {
     try {
       const result = await firstValueFrom(
-        this.client.send('auth.register.user', {}),
+        this.client.send('auth.register.user', registerUserDto),
       );
 
       return result;
@@ -24,10 +28,10 @@ export class AuthController {
   }
 
   @Post('login')
-  async loginUser() {
+  async loginUser(@Body() loginUserDto: LoginUserDto) {
     try {
       const result = await firstValueFrom(
-        this.client.send('auth.login.user', {}),
+        this.client.send('auth.login.user', loginUserDto),
       );
 
       return result;
@@ -36,16 +40,12 @@ export class AuthController {
     }
   }
 
+  @UseGuards(AuthGuard)
   @Get('verify')
-  async verifyToken() {
-    try {
-      const result = await firstValueFrom(
-        this.client.send('auth.verify.token', {}),
-      );
-
-      return result;
-    } catch (error: any) {
-      throw new RpcException(error);
-    }
+  async verifyToken(@User() user: CurrentUser, @Token() token: string) {
+    return {
+      user,
+      token,
+    };
   }
 }
